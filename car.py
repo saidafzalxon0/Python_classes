21d2e724c238dc83de42c549783c74695bda6a42
Shamsiddin Djuraev <shams_@bk.ru>
2023-09-26 10:57:24 +0500

    car qo'shildi

---
+++ car.py
@@ -0,0 +1,12 @@
+class Car:
+    brand='Chevralet'
+    logo='plus'
+    tezlik=250
+    made_in='Uzbekistan'
+
+class Car3 (Car):
+    yoqilgi=elektr
+    def __init__ (self, marka):
+        self.marka=marka
+
+
