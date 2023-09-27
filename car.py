e583fa85441670d5553d146f9e0423ee1f054f9b
Shamsiddin Djuraev <shams_@bk.ru>
2023-09-27 07:52:26 +0500

    restore

--- car.py
+++ car.py
@@ -1,1 +1,27 @@
-63 6c 61 73 73 20 43 61 72 3a 20 20 20 20 20 62 72 61 6e 64 3d 27 43 68 65 76 72 61 6c 65 74 27 20 20 20 20 20 6c 6f 67 6f 3d 27 70 6c 75 73 27 20 20 20 20 20 74 65 7a 6c 69 6b 3d 32 35 30 20 20 20 20 20 6d 61 64 65 5f 69 6e 3d 27 55 7a 62 65 6b 69 73 74 61 6e 27 20 20 20 63 6c 61 73 73 20 43 61 72 32 28 43 61 72 29 3a 20 20 20 20 20 64 65 66 20 5f 5f 69 6e 69 74 5f 5f 28 73 65 6c 66 2c 20 79 6f 71 69 6c 67 69 2c 20 72 61 6e 67 2c 20 6f 74 5f 6b 75 63 68 69 2c 20 29 3a 20 20 20 20 20 20 20 20 20 73 65 6c 66 2e 79 6f 71 69 6c 67 69 20 3d 20 79 6f 71 69 6c 67 69 20 20 20 20 20 20 20 20 20 73 65 6c 66 2e 72 61 6e 67 20 3d 20 72 61 6e 67 20 20 20 20 20 20 20 20 20 73 65 6c 66 2e 6f 74 5f 6b 75 63 68 69 20 3d 20 6f 74 5f 6b 75 63 68 69 20 20 20 20 20 64 65 66 20 63 68 69 71 61 72 28 73 65 6c 66 29 3a 20 20 20 20 20 20 20 20 20 70 72 69 6e 74 28 73 65 6c 66 2e 62 72 61 6e 64 2c 73 65 6c 66 2e 6c 6f 67 6f 2c 73 65 6c 66 2e 74 65 7a 6c 69 6b 2c 73 65 6c 66 2e 6d 61 64 65 5f 69 6e 2c 73 65 6c 66 2e 79 6f 71 69 6c 67 69 2c 73 65 6c 66 2e 72 61 6e 67 2c 73 65 6c 66 2e 6f 74 5f 6b 75 63 68 69 29 20 20 20 63 6c 61 73 73 20 43 61 72 33 20 28 43 61 72 29 3a 20 20 20 20 20 79 6f 71 69 6c 67 69 3d 22 65 6c 65 6b 74 72 22 20 20 20 20 20 64 65 66 20 5f 5f 69 6e 69 74 5f 5f 20 28 73 65 6c 66 2c 20 6d 61 72 6b 61 29 3a 20 20 20 20 20 20 20 20 20 73 65 6c 66 2e 6d 61 72 6b 61 3d 6d 61 72 6b 61 20 20 20 20 20 20 64 65 66 20 63 68 69 71 61 72 28 73 65 6c 66 29 3a 20 20 20 20 20 20 20 20 20 70 72 69 6e 74 28 73 65 6c 66 2e 62 72 61 6e 64 2c 20 73 65 6c 66 2e 6c 6f 67 6f 2c 20 73 65 6c 66 2e 74 65 7a 6c 69 6b 2c 20 73 65 6c 66 2e 6d 61 64 65 5f 69 6e 2c 20 73 65 6c 66 2e 79 6f 71 69 6c 67 69 2c 73 65 6c 66 2e 6d 61 72 6b 61 29 20 63 61 72 32 20 3d 43 61 72 32 28 22 62 65 6e 7a 69 6e 22 2c 22 6f 71 22 2c 37 36 30 29 20 63 61 72 33 20 3d 43 61 72 33 28 22 6c 61 73 65 74 69 22 29 20 63 61 72 32 2e 63 68 69 71 61 72 28 29 20 63 61 72 33 2e 63 68 69 71 61 72 28 29
+class Car:
+    brand='Chevralet'
+    logo='plus'
+    tezlik=250
+    made_in='Uzbekistan'
+
+
+class Car2(Car):
+    def __init__(self, yoqilgi, rang, ot_kuchi, ):
+        self.yoqilgi = yoqilgi
+        self.rang = rang
+        self.ot_kuchi = ot_kuchi
+    def chiqar(self):
+        print(self.brand,self.logo,self.tezlik,self.made_in,self.yoqilgi,self.rang,self.ot_kuchi)
+
+
+class Car3 (Car):
+    yoqilgi="elektr"
+    def __init__ (self, marka):
+        self.marka=marka
+
+    def chiqar(self):
+        print(self.brand, self.logo, self.tezlik, self.made_in, self.yoqilgi,self.marka)
+car2 =Car2("benzin","oq",760)
+car3 =Car3("laseti")
+car2.chiqar()
+car3.chiqar()
