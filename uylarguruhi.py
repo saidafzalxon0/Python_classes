7e68f324df604f1fd9c1dc58775fd8be62885904
Behruz Toshpo'latov <behruzdev08@gmail.com>
2023-09-27 07:46:02 +0500

    Xaxaxaxax

--- uylarguruhi.py
+++ uylarguruhi.py
@@ -36,5 +36,3 @@ class modern_uy(Uy):
         print("Xonalar soni", self.xonalar,"Vannalar soni", self.vanna, "Etajlar soni", self.etaj,"Kamera", self.kamera,"Wifi", self.wifi, "Lift", self.lift, "Parking", self.parking)
 modern_uy = modern_uy("45")
 modern_uy.farqi()
-
-
